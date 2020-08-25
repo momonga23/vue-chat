6d83b423a80de328d63b0332772054cd2d316b14
hmiyachi <hmiyachi@gijuhrdsad.local>
2020-08-25 20:39:58 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
+import './app.css'
 
 Vue.config.productionTip = false
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAAtfjtXDqIalR4IRCiPODjTCv6mYuw0MQ",
+    authDomain: "chat-app-7923c.firebaseapp.com",
+    databaseURL: "https://chat-app-7923c.firebaseio.com",
+    projectId: "chat-app-7923c",
+    storageBucket: "chat-app-7923c.appspot.com",
+    messagingSenderId: "614030290400",
+    appId: "1:614030290400:web:88191511cac1323a1ccf85",
+    measurementId: "G-29E2NFFDR9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 new Vue({
   render: h => h(App),
